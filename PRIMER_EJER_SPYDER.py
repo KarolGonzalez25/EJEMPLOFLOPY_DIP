00a5061c1781b16ecbc2cff104a05bdfc9da20ae
Karol Gonzalez <karol2180580@correo.uis.edu.co>
2021-01-18 17:53:07 -0500

    Add datos principales

--- PRIMER_EJER_SPYDER.py
+++ PRIMER_EJER_SPYDER.py
@@ -7,3 +7,13 @@ This is a temporary script file.
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+
+name = "MOOD_01"
+h1 = 100
+h2 = 90
+Nlay = 10
+N = 101
+L = 400.0
+H = 50.0
+k = 1.0
