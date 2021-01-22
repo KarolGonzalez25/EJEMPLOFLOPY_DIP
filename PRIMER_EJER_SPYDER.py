3935428a3deefdc102f887dc25f8e08adc5c9cf2
Karol Gonzalez <karol2180580@correo.uis.edu.co>
2021-01-22 14:50:08 -0500

    Se añadieron diferentes datos

--- PRIMER_EJER_SPYDER.py
+++ PRIMER_EJER_SPYDER.py
@@ -8,8 +8,10 @@ This is a temporary script file.
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import flopy
 
-name = "MOOD_01"
+#Parametros iniciales
+name = "tutorial01_mf6"
 h1 = 100
 h2 = 90
 Nlay = 10
@@ -18,4 +20,85 @@ L = 400.0
 H = 50.0
 k = 1.0
 
-sim = flopy.mf6.MFSimulation(sim_name=name, exe_name="mf6", version="mf6", sim_ws="." )
+#Buca mf6en la carpeta especificada y guarda los archivos
+sim = flopy.mf6.MFSimulation(sim_name=name, exe_name="C:\WRDAPP\mf6.2.0\bin", version="mf6", sim_ws=".")
+
+#Crea objetos de flopy TDIS
+tdis = flopy.mf6.ModflowTdis(sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)])
+
+#Crea el paquete de objetos de flopy IMS
+ims = flopy.mf6.ModflowIms(sim, pname="ims", complexity="SIMPLE")
+
+#Crea el modelo de flujo de agua
+model_nam_file = "{}.nam".format(name)
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, model_nam_file=model_nam_file)
+
+#Se define valores de espesore fils y paquete de discretización
+#Espesor de filas= delrow
+#Paquetes de discrtización= dis
+bot = np.linspace(-H / Nlay, -H, Nlay)
+delrow = delcol = L / (N - 1)
+dis = flopy.mf6.ModflowGwfdis(
+    gwf,
+    nlay=Nlay,
+    nrow=N,
+    ncol=N,
+    delr=delrow,
+    delc=delcol,
+    top=0.0,
+    botm=bot,
+)
+
+#Entregar el valor de partida para el método númerico
+#Calcular el espesor de la celda 1
+#0 es que asume que el espesor es constante
+start = h1 * np.ones((Nlay, N, N))
+ic = flopy.mf6.ModflowGwfic(gwf, pname="ic", strt=start)
+
+#Controla flujo entre celdas
+npf = flopy.mf6.ModflowGwfnpf(gwf, icelltype=1, k=k, save_flows=True)
+
+#duplas es una lista, que tiene unas coordenadas, 0 capa , 1 fila, 2 columna
+chd_rec = []
+chd_rec.append(((0, int(N / 4), int(N / 4)), h2))
+for layer in range(0, Nlay):
+    for row_col in range(0, N):
+        chd_rec.append(((layer, row_col, 0), h1))
+        chd_rec.append(((layer, row_col, N - 1), h1))
+        if row_col != 0 and row_col != N - 1:
+            chd_rec.append(((layer, 0, row_col), h1))
+            chd_rec.append(((layer, N - 1, row_col), h1))
+chd = flopy.mf6.ModflowGwfchd(
+    gwf,
+    maxbound=len(chd_rec),
+    stress_period_data=chd_rec,
+    save_flows=True,
+)
+
+#
+iper = 0
+ra = chd.stress_period_data.get_data(key=iper)
+ra
+
+# Create the output control (`OC`) Package
+headfile = "{}.hds".format(name)
+head_filerecord = [headfile]
+budgetfile = "{}.cbb".format(name)
+budget_filerecord = [budgetfile]
+saverecord = [("HEAD", "ALL"), ("BUDGET", "ALL")]
+printrecord = [("HEAD", "LAST")]
+oc = flopy.mf6.ModflowGwfoc(
+    gwf,
+    saverecord=saverecord,
+    head_filerecord=head_filerecord,
+    budget_filerecord=budget_filerecord,
+    printrecord=printrecord,
+)
+
+#
+sim.write_simulation()
+
+#
+success, buff = sim.run_simulation()
+if not success:
+    raise Exception("MODFLOW 6 did not terminate normally.")
